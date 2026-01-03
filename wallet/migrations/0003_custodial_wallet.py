c73049ac3b222c29c716715c3f78d153fac8a906
ticketguy <samuelokwu85@gmail.com>
2026-01-03 22:00:52 +0100

    fix: Rename custodial wallet migration to 0003 to avoid conflict

--- wallet/migrations/0003_custodial_wallet.py
+++ wallet/migrations/0003_custodial_wallet.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wallet', '0001_initial'),
+        ('wallet', '0002_alter_walletprofile_options_walletprofile_is_primary_and_more'),
     ]
 
     operations = [
